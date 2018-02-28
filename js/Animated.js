df05905d681476d1d83d8b971b09a4255e7f4339
dunstad <anthony00.blount00@gmail.com>
2018-02-28 14:03:01 +0900

    improved documentation

--- js/Animated.js
+++ js/Animated.js
@@ -33,7 +33,7 @@ class Animated {
   }
 
   /**
-   * 
+   * Performs rotations waiting in the queue.
    */
   process() {
     if (this.queue.length) {
@@ -68,7 +68,7 @@ class Animated {
   }
 
   /**
-   * 
+   * Represents the current transformation state of this as a transformation string.
    */
   getStateString() {
     let bBox = this.element.getBBox();
@@ -76,58 +76,60 @@ class Animated {
   }
 
   /**
-   * 
-   * @param {*} stateChange 
+   * Sends a transformation to the queue to be processed in order.
+   * Important so that transformations which will not be animated
+   * still wait on animated transformations to finish.
+   * @param {object} stateChange 
    */
   sendToQueue(stateChange) {
     stateChange.animate = this.animateQueue;
     this.queue.push(stateChange);
     if (!this.element.inAnim().length) {this.process();}
-    else {console.log('!');}
+    else {console.log('transformation queued');}
     return this;
   }
 
   /**
-   * 
-   * @param {*} x 
-   * @param {*} y 
+   * Moves this to the absolute coordinates provided.
+   * @param {number} x 
+   * @param {number} y 
    */
   move(x, y) {
     return this.sendToQueue({location: {x: x, y: y}});
   }
 
   /**
-   * 
+   * Represents the current location of this as an SVG translation string.
    */
   locationToString() {
     return `t${this.location.x},${this.location.y}`;
   }
 
   /**
-   * 
-   * @param {*} deg 
+   * Rotates this to the absolute degree provided.
+   * @param {number} deg 
    */
   rotate(deg) {
     return this.sendToQueue({rotation: deg});
   }
 
   /**
-   * 
+   * Represents the current rotation of this as an SVG translation string.
    */
   rotationToString() {
     return `r${this.rotation},${this.centerOffsetFromOrigin.x},${this.centerOffsetFromOrigin.y}`;
   }
 
   /**
-   * 
-   * @param {*} ratio 
+   * Scales this to the absolute ratio provided.
+   * @param {number} ratio 
    */
   scale(ratio) {
     return this.sendToQueue({scalar: ratio});
   }
 
   /**
-   * 
+   * Represents the current scale of this as an SVG translation string.
    */
   scalarToString() {
     return `s${this.scalar}`;
