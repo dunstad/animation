61721433a15637d1e8709fc3c928e711aae089fd
Anthony Blount <anthony_blount@co-graph.com>
2018-05-18 17:44:01 +0900

    added some tentative colors

--- js/Sky.js
+++ js/Sky.js
@@ -1,17 +1,28 @@
 class Sky {
   
   constructor(time) {
-    this.time = 0;
-    this.rect;
+    this.timeValue = time;
+    this.rect = svgContainer.rect(10, 10, 100, 100);
     this.gradient;
+    this.skyColors = {
+      5: '#FF5733',
+      6: '#FFC300',
+      7: '#FFE633',
+      12: '#7ec0ee',
+      17: this.skyColors[7],
+      18: this.skyColors[6],
+      19: this.skyColors[5],
+      20: '#3D5FF9',
+      24: '#2300C5',
+    };
   }
 
   get time() {
-
+    return this.timeValue;
   }
 
   set time(time) {
-
+    this.timeValue = time;
   }
 
 }
