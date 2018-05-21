77e717d28fcda78e877d9ad89c2956ec4592ea0e
Anthony Blount <anthony_blount@co-graph.com>
2018-05-21 15:43:37 +0900

    previousHour and nextHour should be useful

--- js/Sky.js
+++ js/Sky.js
@@ -1,7 +1,7 @@
 class Sky {
   
   constructor(time) {
-    this.timeValue = time || 0;
+    this.timeValue = time != undefined ? time : 0;
     
     this.skyColors = {};
     this.skyColors[5] = chroma('#FF5733');
@@ -13,11 +13,26 @@ class Sky {
     this.skyColors[19] = this.skyColors[5];
     this.skyColors[20] = chroma('#3D5FF9');
     this.skyColors[24] = chroma('#2300C5');
+    this.skyColors[0] = this.skyColors[24];
 
     this.gradient = svgContainer.gradient('l(0,0,0,1)');
     this.rect = svgContainer.rect(10, 10, 100, 100);
     this.rect.attr({fill: this.gradient});
     this.setColorFromTime(time);
+  }
+
+  nextHour(time) {
+    let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
+    let result = coloredHours.sort((a, b)=>a-b).find(n=>n>time);
+    result = result != undefined ? result : coloredHours[0];
+    return result;
+  }
+  
+  previousHour(time) {
+    let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
+    let result = coloredHours.sort((a, b)=>b-a).find(n=>n<time);
+    result = result != undefined ? result : coloredHours[0];
+    return result;
   }
 
   get time() {
@@ -30,21 +45,26 @@ class Sky {
   }
 
   setColorFromTime(time) {
-    time = time || this.timeValue;
-    let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n)).sort((a, b)=>a-b);
+    time = time != undefined ? time : this.timeValue;
+    let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
 
-    let bottomHour = coloredHours.find(n=>n>=time) || coloredHours[0];
-    let topHour = coloredHours.find(n=>n<=time) || coloredHours[coloredHours.length - 1];
+    let bottomHour = coloredHours.sort((a, b)=>b-a).find(n=>n<=time);
+    bottomHour = bottomHour != undefined ? bottomHour : coloredHours[coloredHours.length - 1];
+    
+    let topHour = coloredHours.sort((a, b)=>a-b).find(n=>n>=time);
+    topHour = topHour != undefined ? topHour : coloredHours[coloredHours.length - 1];
 
-    let bottomDifference = time - bottomHour;
-    let topDifference = topHour - time;
+    console.log('hours', bottomHour, topHour);
+
+    let bottomDifference = (time - bottomHour) || 1;
+    let topDifference = (topHour - time) || 1;
 
     let range = bottomDifference + topDifference;
 
     let bottomRatio = 1 - (bottomDifference / range);
     let topRatio = 1 - (topDifference / range);
 
-    console.log(bottomRatio, topRatio)
+    console.log('ratios', bottomRatio, topRatio)
 
     let bottomColor = chroma.mix(this.skyColors[bottomHour], this.skyColors[topHour], bottomRatio).hex();
     let topColor = chroma.mix(this.skyColors[bottomHour], this.skyColors[topHour], topRatio).hex();
