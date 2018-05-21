e840b3be5762e6fd5d01a682e7d08dc031b1d0c6
Anthony Blount <anthony_blount@co-graph.com>
2018-05-21 16:53:45 +0900

    sky is relatively useful now

--- js/Sky.js
+++ js/Sky.js
@@ -4,6 +4,8 @@ class Sky {
     this.timeValue = time != undefined ? time : 0;
     
     this.skyColors = {};
+    // best to have a 0 hour and no 24 hour defined
+    this.skyColors[0] = chroma('#2300C5');
     this.skyColors[5] = chroma('#FF5733');
     this.skyColors[6] = chroma('#FFC300');
     this.skyColors[7] = chroma('#FFE633');
@@ -12,8 +14,6 @@ class Sky {
     this.skyColors[18] = this.skyColors[6];
     this.skyColors[19] = this.skyColors[5];
     this.skyColors[20] = chroma('#3D5FF9');
-    this.skyColors[24] = chroma('#2300C5');
-    this.skyColors[0] = this.skyColors[24];
 
     this.gradient = svgContainer.gradient('l(0,0,0,1)');
     this.rect = svgContainer.rect(10, 10, 100, 100);
@@ -21,6 +21,10 @@ class Sky {
     this.setColorFromTime(time);
   }
 
+  /**
+   * Used to deal with the 24 hour cycle properly.
+   * @param {number} time 
+   */
   nextHour(time) {
     let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
     let result = coloredHours.sort((a, b)=>a-b).find(n=>n>time);
@@ -28,6 +32,10 @@ class Sky {
     return result;
   }
 
+  /**
+   * Used to deal with the 24 hour cycle properly.
+   * @param {number} time 
+   */
   previousHour(time) {
     let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
     let result = coloredHours.sort((a, b)=>b-a).find(n=>n<time);
@@ -40,10 +48,15 @@ class Sky {
   }
 
   set time(time) {
+    time = time % 24;
     this.timeValue = time;
     this.setColorFromTime(time);
   }
 
+  /**
+   * Makes the gradient be the colors it ought to based on the given time.
+   * @param {number} time 
+   */
   setColorFromTime(time) {
     time = time != undefined ? time : this.timeValue;
     let coloredHours = Object.keys(this.skyColors).map(n=>parseInt(n));
@@ -53,8 +66,11 @@ class Sky {
 
     console.log('hours', bottomHour, topHour);
 
+    // topHour || 24 lets the transition to the next day work well
+    // || .1 and || 100 are for when both differences are 0,
+    // so colors continue to change smoothly
     let bottomDifference = (time - bottomHour) || .1;
-    let topDifference = (topHour - time) || 100;
+    let topDifference = ((topHour || 24) - time) || 100;
 
     let range = bottomDifference + topDifference;
 
@@ -63,29 +79,11 @@ class Sky {
 
     console.log('ratios', bottomRatio, topRatio)
 
-    // let bottomerColor = this.skyColors[this.previousHour(bottomHour)];
-    // let topperColor = this.skyColors[this.nextHour(topHour)];
-    
-    // let bottomColor = chroma.mix(bottomerColor, this.skyColors[bottomHour], bottomRatio).hex();
-    // let topColor = chroma.mix(this.skyColors[topHour], topperColor, topRatio).hex();
-    
     let nextColor = this.skyColors[this.nextHour(topHour)];
     let bottomColor = chroma.mix(this.skyColors[bottomHour], this.skyColors[topHour], bottomRatio).hex();
     let topColor = chroma.mix(this.skyColors[topHour], nextColor, bottomRatio).hex();
 
     this.gradient.setStops(`${topColor}-${bottomColor}`);
-    // this.gradient.setStops(`${bottomColor}-${topColor}`);
   }
-
-}
-
-function animateStopColor(stop, color, time) {
-
-  let startColor = chroma(stop.attr()['stop-color']).rgb();
-  
-  Snap.animate(startColor, chroma(color).rgb(), function( val ) {
-      let rgb = `rgb(${val})`;
-      stop.attr('stop-color', rgb);
-  }, time);
 
 }
