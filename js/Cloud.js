c2653e4aac61d3960872b79cbdc66dcde609ec9f
Anthony Blount <anthony_blount@co-graph.com>
2018-07-26 13:17:32 +0900

    let's try making some clouds

---
+++ js/Cloud.js
@@ -0,0 +1,25 @@
+class Cloud extends Animated {
+
+  constructor(svgContainer, numArcs, length, fillColor, borderColor) {
+    
+    // lets us pass two colors as an array to fill with a linear gradient
+    if (Array.isArray(fillColor)) {
+      fillColor = svgContainer.gradient(`l(0,0,0,1)${chroma(fillColor[0]).hex()}-${chroma(fillColor[1]).hex()}`);
+    }
+    
+    super(svgContainer.path(Cloud.makePath(numArcs, length)).attr({fill: fillColor, stroke: borderColor}));
+    
+  }
+
+  /**
+   * Generates path strings used to draw clouds
+   * @param {number} numArcs
+   * @param {number} length 
+   */
+  static makePath(numArcs, length) {
+
+    
+
+  }
+
+}
