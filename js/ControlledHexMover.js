398d0a86913469e4a633b8583f35517c10d6afcd
dunstad <dunstad1@gmail.com>
2019-04-11 12:54:37 +0900

    not using click code anymore

--- js/ControlledHexMover.js
+++ js/ControlledHexMover.js
@@ -64,55 +64,6 @@ class ControlledHexMover extends MusicalHexMover {
     });
 
     this.clickHandlerHexes = [];
-    // this.assignClickHandlers();
-
-  }
-
-  /**
-   * TODO: remove this, clicking hexes to move works poorly
-   * going to implement swipe to move instead
-   */
-  assignClickHandlers() {
-
-    for (let data of this.clickHandlerHexes) {
-      data.node.removeEventListener('click', data.handler);
-    }
-    this.clickHandlerHexes = [];
-    
-    let moves = {
-      moveUpLeft: {q: -1, r: 0},
-      moveUp: {q: 0, r: -1},
-      moveUpRight: {q: 1, r: -1},
-      moveDownLeft: {q: -1, r: 1},
-      moveDown: {q: 0, r: 1},
-      moveDownRight: {q: 1, r: 0},
-    };
-
-    for (let [moveName, offsets] of Object.entries(moves)) {
-      // todo: this code is also in HexMover, should probably refactor it
-      let currentAxial = this.hex.cube();
-      let newAxial = {
-        q: currentAxial.q + offsets.q,
-        r: currentAxial.r + offsets.r,
-      };
-      let offsetHex = this.hex.hexgrid.axialGet(newAxial.q, newAxial.r);
-
-      let handler = ()=>{
-        // this is copied from above too...
-        this[moveName](this.duration, this.config);
-        if (!Object.keys(this.anims).length) {
-          this.process();
-        }
-        this.assignClickHandlers();
-      };
-      
-      offsetHex.hexagon.node.addEventListener('click', handler);
-
-      this.clickHandlerHexes.push({
-        node: offsetHex.hexagon.node,
-        handler: handler,
-      });
-    }
 
   }
 
