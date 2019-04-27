98b2ffcbb369883e8b0f9edcb43cc19980ada093
dunstad <anthony00.blount00@gmail.com>
2019-04-27 10:17:13 +0900

    maybe doing these one at a time will help

--- js/sw.js
+++ js/sw.js
@@ -1,34 +1,35 @@
 self.addEventListener('install', (e)=>{
   e.waitUntil(
     caches.open('file-store').then((cache)=>{
-      return cache.addAll([
-        '/hexgrid.html',
-        '/css/hexgrid.css',
-        '/lib/snap.svg.js',
-        '/lib/vivus.min.js',
-        '/lib/flubber.min.js',
-        '/lib/gif.js',
-        '/lib/gif.worker.js',
-        '/lib/jszip.min.js',
-        '/lib/FileSaver.js',
-        '/lib/honeycomb.min.js',
-        '/lib/Tone.js',
-        '/lib/mousetrap.min.js',
-        '/lib/hammer.min.js',
-        '/js/Transformation.js',
-        '/js/AnimationQueue.js',
-        '/js/Animated.js',
-        '/js/External.js',
-        '/js/HexGrid.js',
-        '/js/HexMover.js',
-        '/js/MusicalHexMover.js',
-        '/js/ControlledHexMover.js',
-        '/js/Metronome.js',
-        '/js/HexMoveQueue.js',
-        '/js/Player.js',
-        '/js/Scene.js',
-        '/js/scenes.js',
-      ]);
+      return Promise.all([
+          '/hexgrid.html',
+          '/css/hexgrid.css',
+          '/lib/snap.svg.js',
+          '/lib/vivus.min.js',
+          '/lib/flubber.min.js',
+          '/lib/gif.js',
+          '/lib/gif.worker.js',
+          '/lib/jszip.min.js',
+          '/lib/FileSaver.js',
+          '/lib/honeycomb.min.js',
+          '/lib/Tone.js',
+          '/lib/mousetrap.min.js',
+          '/lib/hammer.min.js',
+          '/js/Transformation.js',
+          '/js/AnimationQueue.js',
+          '/js/Animated.js',
+          '/js/External.js',
+          '/js/HexGrid.js',
+          '/js/HexMover.js',
+          '/js/MusicalHexMover.js',
+          '/js/ControlledHexMover.js',
+          '/js/Metronome.js',
+          '/js/HexMoveQueue.js',
+          '/js/Player.js',
+          '/js/Scene.js',
+          '/js/scenes.js',
+        ].map((url)=>{cache.add(url);})
+      );
     }).catch(console.error)
   );
  });
