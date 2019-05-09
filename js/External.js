ff96c808c5a3e064e0e568c7e4cb32de811c8178
dunstad <dunstad1@gmail.com>
2019-05-09 16:11:20 +0900

    well this works but it's kinda messy

--- js/External.js
+++ js/External.js
@@ -8,11 +8,20 @@ class External extends Animated {
     
   }
 
+  get rotation() {
+    return this.element.first().transform().rotate;
+  }
+
+  set rotation(degree) {
+    if (typeof degree != 'number') {throw new Error('rotation must be a number');}
+    this.element.first().transform({rotate: degree});
+  }
+
   load() {
 
     return new Promise((resolve, reject)=>{
       fetch(this.svgImagePath).then(res=>res.text()).then((svgText)=>{
-        this.element.svg(svgText);
+        this.element.add(svgContainer.group().svg(svgText));
         this._vivus = new Vivus(this.element.node.firstElementChild, {start: 'manual'});
         this._vivus.finish();
         resolve(this);
