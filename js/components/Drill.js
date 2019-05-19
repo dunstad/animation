37790bd1c66df5fca0724721af6b29e40913b37a
dunstad <anthony00.blount00@gmail.com>
2019-05-19 20:52:14 +0900

    drills can now face nearby crystals

--- js/components/Drill.js
+++ js/components/Drill.js
@@ -4,25 +4,41 @@ class Drill extends Animated {
 
     super(svgContainer.polygon([20, 0, 0, 40, 40, 40]));
     this.element.attr(options);
+
+    this.grid = grid;
     
     let pattern = svgContainer.pattern(80, 10, (add)=>{
-      let color = chroma('gray');
+      let color = chroma('#9E9E9E');
       let style = {stroke: chroma(color).darken(), 'stroke-width': 2, fill: 'transparent'};
       add.rect(80, 10).attr({fill: color});
       add.path('M 20 0 C 30 10 50 10 60 0').attr(style);
     });
     this.element.attr({fill: pattern})
 
-    this.facingToRotation = {
+  }
+
+  faceCrystal() {
+
+    let facingToRotation = {
       'up': 0,
       'right': 90,
       'down': 180,
       'left': 270,
     };
 
-  }
+    let directionToOffset = {
+      'up': [0, -1],
+      'right': [1, 0],
+      'down': [0, 1],
+      'left': [-1, 0],
+    };
 
-  faceCrystal() {
+    for (let [direction, coords] of Object.entries(directionToOffset)) {
+      let tile = this.grid.tile(this.tile.gridX + coords[0], this.tile.gridY + coords[1]);
+      if (tile.occupied && tile.occupied.constructor.name == 'Crystal') {
+        this.rotation = facingToRotation[direction];
+      }
+    }
 
   }
 
