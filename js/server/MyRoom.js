71f5c34411e602d115a0dc7cde3f4735069ce73d
dunstad <dunstad1@gmail.com>
2019-06-07 15:08:42 +0900

    figuring out how colyseus works

--- js/server/MyRoom.js
+++ js/server/MyRoom.js
@@ -1,9 +1,20 @@
 const colyseus = require('colyseus');
 
 exports.MyRoom = class extends colyseus.Room {
-  onInit (options) {}
-  onJoin (client, options) {}
-  onMessage (client, message) {}
-  onLeave (client, consented) {}
-  onDispose() {}
+  onInit (options) {
+    console.log("Room created!", options);
+  }
+  onJoin (client, options) {
+    this.broadcast(`${ client.sessionId } joined.`);
+  }
+  onMessage (client, message) {
+    console.log("BasicRoom received message from", client.sessionId, ":", message);
+    this.broadcast(`(${ client.sessionId }) ${ message }`);
+  }
+  onLeave (client, consented) {
+    this.broadcast(`${ client.sessionId } left.`);
+  }
+  onDispose() {
+    console.log("Dispose Room");
+  }
 }
